9e29ab295a2783a6bd2d85c26feaf80e77018d0e
Cs Bence <bcsaldi18@gmail.com>
2025-07-02 21:36:23 +0200

    Initial Commit

---
+++ TaskTime/TaskTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTimeApp.swift
+//  TaskTime
+//
+//  Created by Bence Családi on 2025. 07. 02..
+//
+
+import SwiftUI
+
+@main
+struct TaskTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
